e373c7351421b8b7a56b66a931f9aad35f6dddfc
Nithin Bharadhwaj <anibha88@gmail.com>
2017-12-25 18:10:51 +0530

    pdf fix

--- app/pdfs/order_pdf.rb
+++ app/pdfs/order_pdf.rb
@@ -3,18 +3,25 @@ class OrderPdf < Prawn::Document
 	    super(top_margin: 70)
 	    @details = details
 	    @view = view
-	    text "SREE LPG (P) LTD.,", align: :center, size: 20
-	    move_down 10
-	    text "Employee code/Name :  Ramesh HR", align: :left, size: 15
-	    move_down 5
-	    text "Date: #{Time.now.strftime("%d/%m/%Y")}", align: :right, size: 15
-	    gen_pdf
-	    line_items
+	    # text "SREE LPG (P) LTD.,", align: :center, size: 20
+	    # move_down 10
+	    # text "Employee code/Name :  Ramesh HR", align: :left, size: 15
+	    # move_down 5
+	    # text "Date: #{Time.now.strftime("%d/%m/%Y")}", align: :right, size: 15
+	    # gen_pdf
+	    # line_items
 	end
 
 	def gen_pdf
 	  	Prawn::Document.generate("#{Rails.root}/lib/attachments/ramesh.pdf") do 
-	  		text "This is auto generated pdf!!!"
+	  		#text "This is auto generated pdf!!!"
+		  	text "SREE LPG (P) LTD.,", align: :center, size: 20
+		    move_down 10
+		    text "Employee code/Name :  Ramesh HR", align: :left, size: 15
+		    move_down 5
+		    text "Date: #{Time.now.strftime("%d/%m/%Y")}", align: :right, size: 15
+		    gen_pdf
+		    line_items
 	  	end
 	end
 
