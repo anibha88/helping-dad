cf111132cbae0ba6c3154a230db6bdae229e453e
Nithin Bharadhwaj <anibha88@gmail.com>
2017-12-26 23:17:57 +0530

    changed to live email

--- app/mailers/story_mailer.rb
+++ app/mailers/story_mailer.rb
@@ -4,8 +4,8 @@ class StoryMailer < ApplicationMailer
     email_attachment = "#{Rails.root}/lib/attachments/ramesh_daily_report.pdf"
     
     # mail to: "nithin@cognitiveclouds.com", subject: "Today's report! Date: #{Time.now.strftime("%d/%m/%Y")}"
-    mail to: "nithin@cognitiveclouds.com", subject: "Today's report!"
-    #mail to: "sreelpg_response@yahoo.co.in", subject: "Today's report!"
+    # mail to: "nithin@cognitiveclouds.com", subject: "Today's report!"
+    mail to: "sreelpg_response@yahoo.co.in", subject: "Today's report!"
     mail.attachments["ramesh_daily_report.pdf"] = File.read(email_attachment)
   end
 end
